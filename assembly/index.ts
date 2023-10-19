ca2064ec3cff327327724ece1d5cee6e55ebe84e
Brandon <brandonjmhunt@gmail.com>
2023-10-19 09:44:35 -0700

    adjusted ABI

--- assembly/index.ts
+++ assembly/index.ts
@@ -33,7 +33,7 @@ import { console } from "@steerprotocol/strategy-utils/assembly/utils/console";
     if (response == ''){
       // return payload
       return `{
-"abi" : [{"inputs": [], "name": "globalState", "outputs": [ { "internalType": "uint160", "name": "price", "type": "uint160" }, { "internalType": "int24", "name": "tick", "type": "int24" }, { "internalType": "int24", "name": "prevInitializedTick", "type": "int24" }, { "internalType": "uint16", "name": "fee", "type": "uint16" }, { "internalType": "uint16", "name": "timepointIndex", "type": "uint16" }, { "internalType": "uint8", "name": "communityFee", "type": "uint8" }, { "internalType": "bool", "name": "unlocked", "type": "bool" } ], "stateMutability": "view", "type": "function"}],
+"abi" : [{"inputs":[],"name":"globalState","outputs":[{"internalType":"uint160","name":"price","type":"uint160"},{"internalType":"int24","name":"tick","type":"int24"},{"internalType":"uint16","name":"fee","type":"uint16"},{"internalType":"uint16","name":"timepointIndex","type":"uint16"},{"internalType":"uint8","name":"communityFeeToken0","type":"uint8"},{"internalType":"uint8","name":"communityFeeToken1","type":"uint8"},{"internalType":"bool","name":"unlocked","type":"bool"}],"stateMutability":"view","type":"function"}],
 "address" : \"` + poolAddress + `\",
 "arguments" : [],
 "method" : "globalState",
